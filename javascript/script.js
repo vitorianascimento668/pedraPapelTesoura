4d5a7d3da92a34d8c4a7914e69de0560dd22b4f9
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2023-03-05 14:07:57 -0300

    Update script.js
    Criei algumas variaveis, uma é o data_num para pega o valor do atributo da jogada do jogador, o data_voce ira servir pra preencher o quadro, o data_robo ira preencher a jogada do robo e o v_sorteado ira sortear os valores pro robo jogar.
    
    Criei uma function jogada_voce que acolhe a jogada do meu jogador, e criei a function  para a jogada do robo, onde agora irei começar a pegar o valor sorteado pra começar a da função para o meu jogo
    
    também coloquei o jogada_voce() e o jogada robo chamando  minhas função

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,17 @@
 let jogada_escolha = document.querySelectorAll(".mao")
+let data_num,data_voce,data_robo, v_sorteado
 
-
+function jogada_voce(){
 jogada_escolha.forEach((jogada)=>{
     jogada.addEventListener("click",()=>{
-        
+        data_num = jogada.getAttribute("data-num")
     })
 })
+}
+
+function jogada_robo(){
+    v_sorteado = Math.random()
+}
+
+jogada_voce()
+jogada_robo()
