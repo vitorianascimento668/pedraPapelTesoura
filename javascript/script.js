3965b591e1a325259dcd206d9ea7c2a451e2c75e
Vitoria Nascimento <vitorianascimentoesilva9@gmail.com>
2023-03-05 15:46:33 -0300

    Criei mais variaveis e tambem criei uma function para mostra o valor escolhido na tela (pedra papel ou tesoura)
    Crie duas variaveis, chamadas imagens_container e joquepo)container, quando a imagens_container estive aparecendo, a joquepo não pode aparecer, e vice e versa, entao no jogada_voce eu ja fiz o teste e coloquei o display none na iamgens container e display flex no joquepo_container quando houver um envento de click, nos meus botoes de pedra papel e tesoura,
    Criei tambem uma function chamada valoresEscolhidos que ira mostra na tela o valor escolhido, porém esse valor somente sera mostrado na tela, quando ouve tambem o envento de click nos itens acima, e eu passei o valor de sorteio ou num-click pra ele.

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,35 @@
 let jogada_escolha = document.querySelectorAll(".mao")
-let data_num,data_voce,data_robo, v_sorteado
+let imagens_container = document.querySelector (".imagens_container")
+let joquepo_container = document.querySelector(".joquepo_container")
+let data_num,data_voce,data_robo, v_sorteado, valor_voce, valor_robo, varvoce = "", varrobo = ""
+
 
 function jogada_voce(){
 jogada_escolha.forEach((jogada)=>{
     jogada.addEventListener("click",()=>{
         data_num = jogada.getAttribute("data-num")
+        valorescolhido(data_num)
+        jogada_robo()
+
+        imagens_container.style.display = "none"
+        joquepo_container.style.display = "flex"
+        
     })
 })
 }
 
 function jogada_robo(){
     v_sorteado = Math.floor(Math.random() * (3-0)+ 0)
+    valorescolhido(v_sorteado)
+}
+function valorescolhido(valor_voce) {
+   if (varvoce.length == 0){
+        varvoce =  valor_voce
+   }setInterval (()=>{
+    if (varrobo.length > 0 ) {
+        varrobo = valor_voce
+    }
+   },'100')
 }
 
 jogada_voce()
-jogada_robo()
